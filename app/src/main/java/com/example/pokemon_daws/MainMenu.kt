a6cecdd606b364018fbacb4ed905bebcdca19bf0
Raagav Prasanna <raagav.pras@gmail.com>
2022-11-11 21:52:37 -0500

    Completed layout

---
+++ app/src/main/java/com/example/pokemon_daws/MainMenu.kt
@@ -0,0 +1,16 @@
+package com.example.pokemon_daws
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pokemon_daws.databinding.ActivityMainMenuBinding
+
+class MainMenu : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainMenuBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
