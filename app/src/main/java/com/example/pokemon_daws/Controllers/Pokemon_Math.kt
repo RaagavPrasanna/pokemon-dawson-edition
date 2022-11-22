63b976c85995be4e46cb6d8450a3f3b8c71d4fc0
Aidan <aidanboch@gmail.com>
2022-11-22 10:07:34 -0500

    Removed relations from damage calculations, it will require a Singleton instead of data to be passed.

--- app/src/main/java/com/example/pokemon_daws/Controllers/Pokemon_Math.kt
+++ app/src/main/java/com/example/pokemon_daws/Controllers/Pokemon_Math.kt
@@ -13,47 +13,48 @@ import android.util.Log
 // Math formulas that affect Pokemon behaviour.
 public final class Pokemon_Math () {
     fun CalculateDamage(attackerLevel: Double, attackerAttack: Double, defenderDefense: Double, movePower: Double, moveType: Type, attackerTypes: List<Type>
-, attackerRelations: TypeRelations, defenderTypes: List<Type>): Int {
+, defenderTypes: List<Type>): Int {
         val levelContribution = ((2 * attackerLevel) / 5 + 2) / 50;
         val attDefRatio = attackerAttack / defenderDefense;
         var damage = (levelContribution * movePower * attDefRatio + 2);
-        var effectiveness: String?;
+        var effectiveness: String? = null;
 
         if (moveType in attackerTypes) {
             damage = damage * 1.5;
         }
-        Log.i("After move type",damage.toString())
-        defenderTypes.forEach{
-            val defenderType = it
-            when(defenderType) {
-                Type.BUG -> effectiveness = attackerRelations.bug;
-                Type.DRAGON -> effectiveness = attackerRelations.dragon;
-                Type.ELECTRIC -> effectiveness = attackerRelations.electric;
-                Type.FIGHTING -> effectiveness = attackerRelations.fighting;
-                Type.FIRE -> effectiveness = attackerRelations.fire;
-                Type.NORMAL -> effectiveness = attackerRelations.normal;
-                Type.FLYING -> effectiveness = attackerRelations.flying;
-                Type.GHOST -> effectiveness = attackerRelations.ghost;
-                Type.GRASS -> effectiveness = attackerRelations.grass;
-                Type.GROUND -> effectiveness = attackerRelations.ground;
-                Type.ICE -> effectiveness = attackerRelations.ice;
-                Type.POISON -> effectiveness = attackerRelations.poison;
-                Type.PSYCHIC -> effectiveness = attackerRelations.psychic;
-                Type.ROCK -> effectiveness = attackerRelations.rock;
-                Type.WATER -> effectiveness = attackerRelations.water;
-            }
-            if (effectiveness == "not_very_effective") {
-                damage = damage / 2;
-
-            }
-            else if (effectiveness == "super_effective") {
-                damage = damage * 2;
-            }
-            else if (effectiveness == "no_effect"){
-                damage = 0.0;
-            }
-            Log.i("Effectiveness",effectiveness + " on " + defenderType.toString())
-        }
+        //TODO(Implement type relations once the singleton has been created)
+        //Log.i("After move type",damage.toString())
+        //defenderTypes.forEach{
+        //    val defenderType = it
+        //    when(defenderType) {
+        //        Type.BUG -> effectiveness = moveRelations.bug;
+        //        Type.DRAGON -> effectiveness = moveRelations.dragon;
+        //        Type.ELECTRIC -> effectiveness = moveRelations.electric;
+        //        Type.FIGHTING -> effectiveness = moveRelations.fighting;
+        //        Type.FIRE -> effectiveness = moveRelations.fire;
+        //        Type.NORMAL -> effectiveness = moveRelations.normal;
+        //        Type.FLYING -> effectiveness = moveRelations.flying;
+        //        Type.GHOST -> effectiveness = moveRelations.ghost;
+        //        Type.GRASS -> effectiveness = moveRelations.grass;
+        //        Type.GROUND -> effectiveness = moveRelations.ground;
+        //        Type.ICE -> effectiveness = moveRelations.ice;
+        //        Type.POISON -> effectiveness = moveRelations.poison;
+        //        Type.PSYCHIC -> effectiveness = moveRelations.psychic;
+        //        Type.ROCK -> effectiveness = moveRelations.rock;
+        //        Type.WATER -> effectiveness = moveRelations.water;
+        //    }
+        //    if (effectiveness == "not_very_effective") {
+        //        damage = damage / 2;
+//
+        //    }
+        //    else if (effectiveness == "super_effective") {
+        //        damage = damage * 2;
+        //    }
+        //    else if (effectiveness == "no_effect"){
+        //        damage = 0.0;
+        //    }
+        //    Log.i("Effectiveness",effectiveness + " on " + defenderType.toString())
+        //}
 
         val finalDamage: Int = damage.roundToInt();
 
