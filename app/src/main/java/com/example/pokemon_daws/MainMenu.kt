5e9dfcfae7b2a4bced3cdef7a4e0e49a5a5ff11e
Vinuyan Sivakolunthu <vinuyan.sivakolunthu@dawsoncollege.qc.ca>
2022-12-09 00:56:19 -0500

    added fragmentt o show battle menu

--- app/src/main/java/com/example/pokemon_daws/MainMenu.kt
+++ app/src/main/java/com/example/pokemon_daws/MainMenu.kt
@@ -1,8 +1,10 @@
 package com.example.pokemon_daws
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.pokemon_daws.databinding.ActivityMainMenuBinding
+import com.example.pokemon_daws.fragments.WildBattle
 
 class MainMenu : AppCompatActivity() {
 
@@ -12,5 +14,10 @@ class MainMenu : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainMenuBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.wildEncounterButton.setOnClickListener{
+            val wildBattleIntent = Intent(this, WildBattle::class.java)
+            startActivity(wildBattleIntent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/pokemon_daws/fragments/BattleMessage.kt
@@ -0,0 +1,17 @@
+package com.example.pokemon_daws.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.pokemon_daws.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [BattleMessage.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class BattleMessage : Fragment(R.layout.fragment_battle_message) {
+
+}
