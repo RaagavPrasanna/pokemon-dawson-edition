353bcd91bd1f9c0ab02f3b1ec21522254aefef83
Vinuyan Sivakolunthu <vinuyan.sivakolunthu@dawsoncollege.qc.ca>
2022-12-09 08:44:04 -0500

    closed reader in fetching, removed unecessary lifecyclescope in fetching and added fragments

--- app/src/main/java/com/example/pokemon_daws/fragments/MoveMenu.kt
+++ app/src/main/java/com/example/pokemon_daws/fragments/MoveMenu.kt
@@ -1,12 +1,12 @@
 package com.example.pokemon_daws.fragments
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.pokemon_daws.R
 
-class BattleMenu : Fragment(R.layout.fragment_battle_menu) {
+
+class MoveMenu : Fragment(R.layout.fragment_move_menu) {
 }
