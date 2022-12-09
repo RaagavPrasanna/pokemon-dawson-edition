e211dc6b7bd75097587248f480794af5567c408b
Vinuyan Sivakolunthu <vinuyan.sivakolunthu@dawsoncollege.qc.ca>
2022-12-09 09:18:46 -0500

    added click listeners on all battle menu btns

--- app/src/main/java/com/example/pokemon_daws/fragments/BattleMenu.kt
+++ app/src/main/java/com/example/pokemon_daws/fragments/BattleMenu.kt
@@ -6,7 +6,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
 import com.example.pokemon_daws.R
+import com.example.pokemon_daws.databinding.FragmentBattleMenuBinding
+import com.example.pokemon_daws.databinding.FragmentItemsMenuBinding
 
 class BattleMenu : Fragment(R.layout.fragment_battle_menu) {
+    private lateinit var binding: FragmentBattleMenuBinding
+    val moveMenu = MoveMenu()
+    val itemMenu = ItemsMenu()
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentBattleMenuBinding.bind(view)
+
+        binding.fightBtn.setOnClickListener{
+            switchFragment(moveMenu)
+        }
+
+        binding.itemBtn.setOnClickListener{
+            switchFragment(itemMenu)
+        }
+
+        binding.runBtn.setOnClickListener {
+            activity?.finish()
+        }
+
+        //TODO add Pokemon team picking
+    }
+
+    private fun switchFragment(fragment:Fragment){
+        val fragManager = requireActivity().supportFragmentManager
+        val transac = fragManager.beginTransaction()
+        transac.replace(R.id.battle_menu, fragment)
+            .addToBackStack("Battle Menu")
+            .commit()
+    }
 }
