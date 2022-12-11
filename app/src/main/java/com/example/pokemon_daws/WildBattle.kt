f9e4c73e6657c0fa46f248ada1ea8b5a9a560f8f
Raagav Prasanna <raagav.pras@gmail.com>
2022-12-10 20:22:28 -0500

    Refactored wild battle to use binding

--- app/src/main/java/com/example/pokemon_daws/WildBattle.kt
+++ app/src/main/java/com/example/pokemon_daws/WildBattle.kt
@@ -4,20 +4,34 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.lifecycleScope
 import com.example.pokemon_daws.Controllers.Battle
+import com.example.pokemon_daws.databinding.ActivityWildBattleBinding
 import com.example.pokemon_daws.fragments.BattleMenu
 import com.example.pokemon_daws.fragments.BattleScreen
 import com.example.pokemon_daws.fragments.BattleText
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class WildBattle : AppCompatActivity() {
     private lateinit var screenFrag : BattleScreen
     private lateinit var menuFrag: BattleMenu
     val textFrag = BattleText()
     public lateinit var battle: Battle
+    private lateinit var binding: ActivityWildBattleBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_wild_battle)
+
+        binding = ActivityWildBattleBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
         screenFrag = BattleScreen.newInstance(this)
+
+
+        lifecycleScope.launch(Dispatchers.IO) {
+
+        }
 
         supportFragmentManager.beginTransaction().apply {
             this.add(R.id.battle_screen, screenFrag)
