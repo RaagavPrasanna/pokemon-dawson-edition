84c740f6ea6c5cda95569f611278121f842f3e8b
Raagav Prasanna <raagav.pras@gmail.com>
2022-12-10 21:26:36 -0500

    Loading works

--- app/src/main/java/com/example/pokemon_daws/WildBattle.kt
+++ app/src/main/java/com/example/pokemon_daws/WildBattle.kt
@@ -2,6 +2,7 @@ package com.example.pokemon_daws
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.lifecycle.lifecycleScope
 import com.example.pokemon_daws.Controllers.Battle
 import com.example.pokemon_daws.databinding.ActivityWildBattleBinding
@@ -10,6 +11,7 @@ import com.example.pokemon_daws.fragments.BattleScreen
 import com.example.pokemon_daws.fragments.BattleText
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class WildBattle : AppCompatActivity() {
     private lateinit var screenFrag : BattleScreen
@@ -24,13 +26,26 @@ class WildBattle : AppCompatActivity() {
         binding = ActivityWildBattleBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        binding.battleScreen.visibility = View.GONE
+        binding.battleMenu.visibility = View.GONE
+        binding.battleMessage.visibility = View.GONE
+        binding.loadBar.visibility = View.VISIBLE
 
         screenFrag = BattleScreen.newInstance(this)
-
+        battle = Battle(lifecycleScope/*, screenFrag*/)
 
         lifecycleScope.launch(Dispatchers.IO) {
-
+            battle.initBattle()
+            while(!battle.gotPk) {
+                println("hasnt got")
+            }
+            withContext(Dispatchers.Main) {
+                binding.loadBar.visibility = View.GONE
+                binding.battleScreen.visibility = View.VISIBLE
+                binding.battleMenu.visibility = View.VISIBLE
+                binding.battleMessage.visibility = View.VISIBLE
+                screenFrag.updateScreen(battle.opponentPk)
+            }
         }
 
         supportFragmentManager.beginTransaction().apply {
@@ -39,7 +54,6 @@ class WildBattle : AppCompatActivity() {
             commit()
         }
 
-        battle = Battle(lifecycleScope, screenFrag)
         menuFrag = BattleMenu.newInstance(this, battle)
         supportFragmentManager.beginTransaction().apply {
             this.add(R.id.battle_menu, menuFrag)
